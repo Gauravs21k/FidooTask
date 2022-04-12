78c4deb8d21bdb0ceccba0472d7941dfedb8f352
Gaurav Singh <gauravs21k@gmail.com>
2022-04-12 14:10:21 +0530

    PostListFragment created

--- app/src/main/java/com/kaimiya/gaurav/fidoointerviewtask/MainActivity.kt
+++ app/src/main/java/com/kaimiya/gaurav/fidoointerviewtask/MainActivity.kt
@@ -2,10 +2,21 @@ package com.kaimiya.gaurav.fidoointerviewtask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setFragment()
+    }
+
+    private fun setFragment() {
+        val fragment = PostListFragment.newInstance()
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(android.R.id.content, fragment)
+        fragmentTransaction.commit()
     }
 }
